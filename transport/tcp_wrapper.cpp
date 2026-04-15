90540ffc5f57878c948faeb2fa287e87eff443f3
lori521 <lorena.tanase@stud.acs.upb.ro>
2026-04-15 13:53:16 +0300

    Added: tcp_wrapper.cpp

---
+++ transport/tcp_wrapper.cpp
@@ -0,0 +1,5 @@
+// TODO -- >> plan de bataie
+/* wrapp -> send */
+/* unwrapp -> receive */
+/* add inside packet ipv4 layer */
+/* modify establish connection/ finish connection with ipv4 send/receive logic */
