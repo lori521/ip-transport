55ccc8d496f819270125b61e8954ae049a766648
lori521 <lorena.tanase@stud.acs.upb.ro>
2026-04-16 12:39:55 +0300

    Added: state updates for each handshake

--- transport/tcp_state_machine.cpp
+++ transport/tcp_state_machine.cpp
@@ -6,13 +6,15 @@
 // -> send ACK-SYN (ack#B, syn#A + 1) 
 // -> receive ACK (ack#B + 1)
 // -> print connection established server for debugging
-bool tcp_packet::establish_connection_receiver(int socketfd, tcp_pseudoheader *pshdr_addr, uint16_t dest_port, uint16_t src_port) {
+bool tcp_layer::establish_connection_receiver(int socketfd, tcp_pseudoheader *pshdr_addr, uint16_t dest_port, uint16_t src_port) {
     uint8_t *temp_buffer = static_cast<uint8_t *>(calloc(PAYLOAD_LENGTH + sizeof(tcp_header), sizeof(uint8_t)));
 
     // wait to receive from sender first
     struct sockaddr_in sender_addr;
     socklen_t sender_length = sizeof(sender_addr);
 
+    // update state 
+    this->current_state = LISTEN;
     // hopefully receiving syn
     // printf("[SERVER] waiting for SYN...\n"); fflush(stdout);
     int rc = recvfrom(socketfd, temp_buffer, PAYLOAD_LENGTH + sizeof(tcp_header), 0, (struct sockaddr*)&sender_addr, &sender_length);
@@ -25,9 +27,13 @@ bool tcp_packet::establish_connection_receiver(int socketfd, tcp_pseudoheader *p
         return false;
     }
 
+    // update state again
+    this->current_state = SYN_RECEIVED;
+
     bool decap_check;
-    decap_check = decapsulate_package(pshdr_addr, temp_buffer, rc);
-    // printf("[SERVER] SYN decapsulated, flag = %d\n", this->tcp_hdr.get_flag()); fflush(stdout);
+    tcp_packet received_packet;
+    decap_check = received_packet.decapsulate_package(pshdr_addr, temp_buffer, rc);
+    // printf("[SERVER] SYN decapsulated, flag = %d\n", received_packet->tcp_hdr.get_flag()); fflush(stdout);
 
     // sanity check for decapsulation
     if (!decap_check) {
@@ -36,18 +42,19 @@ bool tcp_packet::establish_connection_receiver(int socketfd, tcp_pseudoheader *p
         return false;
     }
     // sanity check for syn
-    if (!(this->tcp_hdr.get_flag() & TCP_SYN)) {
+    if (!(received_packet.tcp_hdr.get_flag() & TCP_SYN)) {
         printf("could not receive syn :/\n");
         free(temp_buffer);
         return false;
     }
+
 
     // generate new sequence number for syn-ack packet
     uint32_t seq_number = generate_random_sequence_number();
     // make new header for packet
     tcp_header syn_ack_header = tcp_header(src_port, dest_port);
     syn_ack_header.set_flag(TCP_SYN | TCP_ACK);
-    syn_ack_header.set_ack_number(this->tcp_hdr.get_sequence() + 1);
+    syn_ack_header.set_ack_number(received_packet.tcp_hdr.get_sequence() + 1);
     syn_ack_header.set_sequence(seq_number);
 
     // make new packet with no payload
@@ -62,6 +69,7 @@ bool tcp_packet::establish_connection_receiver(int socketfd, tcp_pseudoheader *p
     sendto(socketfd, send_buffer, package_length, 0, (struct sockaddr*)&sender_addr, sender_length);
     // printf("[SERVER] SYN-ACK sent\n"); fflush(stdout);
     free(send_buffer);
+    syn_ack_packet.free_package();
 
     // receive last packet
     memset(temp_buffer, 0, PAYLOAD_LENGTH + sizeof(tcp_header));
@@ -76,7 +84,7 @@ bool tcp_packet::establish_connection_receiver(int socketfd, tcp_pseudoheader *p
         return false;
     }
 
-    decap_check = decapsulate_package(pshdr_addr, temp_buffer, rc);
+    decap_check = received_packet.decapsulate_package(pshdr_addr, temp_buffer, rc);
     // sanity check for decapsulation
     if (!decap_check) {
         printf("could not decapsulate syn packet :/\n");
@@ -84,14 +92,14 @@ bool tcp_packet::establish_connection_receiver(int socketfd, tcp_pseudoheader *p
         return false;
     }
     // sanity check for ack
-    if (!(this->tcp_hdr.get_flag() & TCP_ACK)) {
+    if (!(received_packet.tcp_hdr.get_flag() & TCP_ACK)) {
         printf("could not receive syn :/\n");
         free(temp_buffer);
         return false;
     }
 
     // check sequence
-    if (this->tcp_hdr.get_ack_number() != seq_number + 1) {
+    if (received_packet.tcp_hdr.get_ack_number() != seq_number + 1) {
         printf("ACK number is incorrect :/ (receiver)\n");
         free(temp_buffer);
         return false;
@@ -99,6 +107,9 @@ bool tcp_packet::establish_connection_receiver(int socketfd, tcp_pseudoheader *p
 
     printf("receiver connection was established ^^\n");
     free(temp_buffer);
+
+    // update state
+    this->current_state = ESTABLISHED;
 
     return true;
 }
@@ -108,7 +119,7 @@ bool tcp_packet::establish_connection_receiver(int socketfd, tcp_pseudoheader *p
 // -> receive SYN (seq#A + 1)
 // -> receive ACK (seq#B + 1)
 // -> send ACK (seq#B + 1)
-bool tcp_packet::establish_connection_sender(int socketfd, tcp_pseudoheader *pshdr_addr, uint16_t dest_port, uint16_t src_port, struct sockaddr_in *receiver_addr) {
+bool tcp_layer::establish_connection_sender(int socketfd, tcp_pseudoheader *pshdr_addr, uint16_t dest_port, uint16_t src_port, struct sockaddr_in *receiver_addr) {
     // send first syn to receiver
     // generate new sequence number for syn packet
     uint32_t seq_number = generate_random_sequence_number();
@@ -128,7 +139,11 @@ bool tcp_packet::establish_connection_sender(int socketfd, tcp_pseudoheader *psh
     // printf("[CLIENT] sending SYN...\n"); fflush(stdout);
     sendto(socketfd, send_buffer, package_length, 0, (struct sockaddr*)receiver_addr, sizeof(* receiver_addr));
     // printf("[CLIENT] SYN sent, waiting for SYN-ACK...\n"); fflush(stdout);
+    // update state
+    this->current_state = SYN_SENT;
+
     free(send_buffer);
+    syn_packet.free_package();
 
     // new buffer to receive syn-ack packet
     uint8_t *temp_buffer = static_cast<uint8_t *>(calloc(PAYLOAD_LENGTH + sizeof(tcp_header), sizeof(uint8_t)));
@@ -145,7 +160,8 @@ bool tcp_packet::establish_connection_sender(int socketfd, tcp_pseudoheader *psh
     }
 
     bool decap_check;
-    decap_check = decapsulate_package(pshdr_addr, temp_buffer, rc);
+    tcp_packet received_packet;
+    decap_check = received_packet.decapsulate_package(pshdr_addr, temp_buffer, rc);
     // sanity check for decapsulation
     if (!decap_check) {
         printf("could not decapsulate syn-ack packet :/\n");
@@ -154,21 +170,21 @@ bool tcp_packet::establish_connection_sender(int socketfd, tcp_pseudoheader *psh
     }
 
     // sanity check for syn-ack
-    if ((this->tcp_hdr.get_flag() & (TCP_SYN | TCP_ACK)) != (TCP_SYN | TCP_ACK)) {
+    if ((received_packet.tcp_hdr.get_flag() & (TCP_SYN | TCP_ACK)) != (TCP_SYN | TCP_ACK)) {
         printf("could not receive syn-ack :/\n");
         free(temp_buffer);
         return false;
     }
 
     // check sequence
-    if (this->tcp_hdr.get_ack_number() != seq_number + 1) {
+    if (received_packet.tcp_hdr.get_ack_number() != seq_number + 1) {
         printf("SYN-ACK number is incorrect :/ (sender)\n");
         free(temp_buffer);
         return false;
     }
 
     // save sequence number to check next packet 
-    uint32_t check_sequence = this->tcp_hdr.get_sequence();
+    uint32_t check_sequence = received_packet.tcp_hdr.get_sequence();
 
     // generate new sequence number for ack packet
     seq_number = generate_random_sequence_number();
@@ -188,10 +204,13 @@ bool tcp_packet::establish_connection_sender(int socketfd, tcp_pseudoheader *psh
     // send first ack packet
     sendto(socketfd, send_buffer, package_length, 0, (struct sockaddr*)receiver_addr, sizeof(* receiver_addr));
     free(send_buffer);
+    ack_packet.free_package();
 
     printf("sender connection was established ^^\n");
     free(temp_buffer);
 
+    // update state
+    this->current_state = ESTABLISHED;
     return true;
 }
 
@@ -202,7 +221,7 @@ bool tcp_packet::establish_connection_sender(int socketfd, tcp_pseudoheader *psh
 // -> send FIN (fin #n)
 // -> receive ACK (ack #n + 1)
 // -> print finished connection receiver for debugging
-bool tcp_packet::finish_connection_receiver(int socketfd, tcp_pseudoheader *pshdr_addr, uint16_t dest_port, uint16_t src_port) {
+bool tcp_layer::finish_connection_receiver(int socketfd, tcp_pseudoheader *pshdr_addr, uint16_t dest_port, uint16_t src_port) {
     // new buffer to receive fin packet
     uint8_t *temp_buffer = static_cast<uint8_t *>(calloc(PAYLOAD_LENGTH + sizeof(tcp_header), sizeof(uint8_t)));
 
@@ -223,7 +242,8 @@ bool tcp_packet::finish_connection_receiver(int socketfd, tcp_pseudoheader *pshd
     }
 
     bool decap_check;
-    decap_check = decapsulate_package(pshdr_addr, temp_buffer, rc);
+    tcp_packet received_packet;
+    decap_check = received_packet.decapsulate_package(pshdr_addr, temp_buffer, rc);
     // sanity check for decapsulation
     if (!decap_check) {
         printf("could not decapsulate fin packet :/\n");
@@ -232,7 +252,7 @@ bool tcp_packet::finish_connection_receiver(int socketfd, tcp_pseudoheader *pshd
     }
 
     // check to see if flag is fin
-    if (!(this->tcp_hdr.get_flag() & TCP_FIN)) {
+    if (!(received_packet.tcp_hdr.get_flag() & TCP_FIN)) {
         printf("fin was not received :/\n");
         free(temp_buffer);
         return false;
@@ -240,7 +260,7 @@ bool tcp_packet::finish_connection_receiver(int socketfd, tcp_pseudoheader *pshd
 
     // send ack packet with and ack_number m + 1
     // save sequence number to check next packet 
-    uint32_t check_sequence = this->tcp_hdr.get_sequence();
+    uint32_t check_sequence = received_packet.tcp_hdr.get_sequence();
 
     // make new header
     tcp_header ack_header = tcp_header(src_port, dest_port);
@@ -257,6 +277,10 @@ bool tcp_packet::finish_connection_receiver(int socketfd, tcp_pseudoheader *pshd
     // send ack package to sender
     sendto(socketfd, send_buffer, package_length, 0, (struct sockaddr*)&sender_addr, sender_length);
     free(send_buffer);
+    ack_packet.free_package();
+
+    // update state
+    this->current_state = CLOSE_WAIT;
 
     // send fin packet with and new seq 
     // save sequence number to check next packet 
@@ -276,6 +300,7 @@ bool tcp_packet::finish_connection_receiver(int socketfd, tcp_pseudoheader *pshd
     // send fin package to sender
     sendto(socketfd, send_buffer, package_length, 0, (struct sockaddr*)&sender_addr, sender_length);
     free(send_buffer);
+    fin_packet.free_package();
 
     // hopefully getting ack n + 1 to finish connection
     memset(temp_buffer, 0, PAYLOAD_LENGTH + sizeof(tcp_header));
@@ -289,7 +314,7 @@ bool tcp_packet::finish_connection_receiver(int socketfd, tcp_pseudoheader *pshd
     }
 
     // decapsulate package
-    decap_check = decapsulate_package(pshdr_addr, temp_buffer, rc);
+    decap_check = received_packet.decapsulate_package(pshdr_addr, temp_buffer, rc);
     // sanity check for decapsulation
     if (!decap_check) {
         printf("could not decapsulate ack packet :/\n");
@@ -298,14 +323,16 @@ bool tcp_packet::finish_connection_receiver(int socketfd, tcp_pseudoheader *pshd
     }
 
     // check to see if flag is set to ack
-    if (!(this->tcp_hdr.get_flag() & TCP_ACK)) {
+    if (!(received_packet.tcp_hdr.get_flag() & TCP_ACK)) {
         printf("ack n + 1 was not received :/\n");
         free(temp_buffer);
         return false;
     }
+    // update state
+    this->current_state = LAST_ACK;
 
     // sequence number check
-    if (this->tcp_hdr.get_ack_number() != new_seq_number + 1) {
+    if (received_packet.tcp_hdr.get_ack_number() != new_seq_number + 1) {
         printf("ACK number is incorrect :/ (receiver)\n");
         free(temp_buffer);
         return false;
@@ -313,6 +340,9 @@ bool tcp_packet::finish_connection_receiver(int socketfd, tcp_pseudoheader *pshd
 
     printf("receiver connection was finished ^^\n");
     free(temp_buffer);
+
+    // update state
+    this->current_state = CLOSED;
 
     return true;
 }
@@ -323,7 +353,7 @@ bool tcp_packet::finish_connection_receiver(int socketfd, tcp_pseudoheader *pshd
 // -> receive FIN (fin #n)
 // -> send ACK (ack #n + 1)
 // -> print finished connection sebder for debugging
-bool tcp_packet::finish_connection_sender(int socketfd, tcp_pseudoheader *pshdr_addr, uint16_t dest_port, uint16_t src_port, struct sockaddr_in *receiver_addr) {
+bool tcp_layer::finish_connection_sender(int socketfd, tcp_pseudoheader *pshdr_addr, uint16_t dest_port, uint16_t src_port, struct sockaddr_in *receiver_addr) {
     // send fin packet with m as sequence number 
     // save sequence number to check next packet 
     uint32_t new_seq_number = generate_random_sequence_number();
@@ -342,6 +372,10 @@ bool tcp_packet::finish_connection_sender(int socketfd, tcp_pseudoheader *pshdr_
     // send fin package to sender
     sendto(socketfd, send_buffer, package_length, 0, (struct sockaddr*)receiver_addr, sizeof(* receiver_addr));
     free(send_buffer);
+    fin_packet.free_package();
+
+    // update state
+    this->current_state = FIN_WAIT_1;
 
     // receive ack with sequence number ack m + 1
     uint8_t *temp_buffer = static_cast<uint8_t *>(calloc(PAYLOAD_LENGTH + sizeof(tcp_header), sizeof(uint8_t)));
@@ -360,7 +394,8 @@ bool tcp_packet::finish_connection_sender(int socketfd, tcp_pseudoheader *pshdr_
     }
 
     bool decap_check;
-    decap_check = decapsulate_package(pshdr_addr, temp_buffer, rc);
+    tcp_packet received_packet;
+    decap_check = received_packet.decapsulate_package(pshdr_addr, temp_buffer, rc);
     // sanity check for decapsulation
     if (!decap_check) {
         printf("could not decapsulate ack m + 1 packet :/\n");
@@ -369,17 +404,20 @@ bool tcp_packet::finish_connection_sender(int socketfd, tcp_pseudoheader *pshdr_
     }
 
     // check to see if flag is fin
-    if (!(this->tcp_hdr.get_flag() & TCP_ACK)) {
+    if (!(received_packet.tcp_hdr.get_flag() & TCP_ACK)) {
         printf("ack m + 1 was not received :/\n");
         free(temp_buffer);
         return false;
     }
 
-    if (this->tcp_hdr.get_ack_number() != new_seq_number + 1) {
+    if (received_packet.tcp_hdr.get_ack_number() != new_seq_number + 1) {
         printf("ack m + 1 could not match\n");
         free(temp_buffer);
         return false;
     }
+    
+    // update state
+    this->current_state = FIN_WAIT_2;
 
     // receive fin n
     memset(temp_buffer, 0,  PAYLOAD_LENGTH + sizeof(tcp_header));
@@ -393,7 +431,7 @@ bool tcp_packet::finish_connection_sender(int socketfd, tcp_pseudoheader *pshdr_
         return false;
     }
 
-    decap_check = decapsulate_package(pshdr_addr, temp_buffer, rc);
+    decap_check = received_packet.decapsulate_package(pshdr_addr, temp_buffer, rc);
     // sanity check for decapsulation
     if (!decap_check) {
         printf("could not decapsulate fin n packet :/\n");
@@ -402,14 +440,14 @@ bool tcp_packet::finish_connection_sender(int socketfd, tcp_pseudoheader *pshdr_
     }
 
     // check to see if flag is fin
-    if (!(this->tcp_hdr.get_flag() & TCP_FIN)) {
+    if (!(received_packet.tcp_hdr.get_flag() & TCP_FIN)) {
         printf("fin n was not received :/\n");
         free(temp_buffer);
         return false;
     }
 
     // send final ack n + 1
-    uint32_t check_sequence = this->tcp_hdr.get_sequence();
+    uint32_t check_sequence = received_packet.tcp_hdr.get_sequence();
 
     // make new header
     tcp_header ack_header = tcp_header(src_port, dest_port);
@@ -426,9 +464,13 @@ bool tcp_packet::finish_connection_sender(int socketfd, tcp_pseudoheader *pshdr_
     // send ack package to sender
     sendto(socketfd, send_buffer, package_length, 0, (struct sockaddr*)receiver_addr, sizeof(* receiver_addr));
     free(send_buffer);
+    ack_packet.free_package();
 
     printf("sender connection was finished ^^\n");
     free(temp_buffer);
 
+    // update state
+    this->current_state = TIME_WAIT;
+    
     return true;
 }
